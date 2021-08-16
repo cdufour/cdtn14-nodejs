8f09e364742052980dbe17d84893e64c004ac2ca
cdufour <opusidea@gmail.com>
2021-08-16 15:41:05 +0200

    j1 tp

--- node-notes/index.js
+++ node-notes/index.js
@@ -1,16 +1,50 @@
 const yargs = require('yargs');
-const { addNote } = require('./notes');
+const { addNote, fetchNotes, editNote, removeNote } = require('./notes');
 
-const argv = yargs.command('add', 'Ajouter une note', {
-  title: {
-    describe: 'Titre de la note',
-    alias: 't',
-    required: true
-  }
-}).help().argv;
+const titleOption = {
+  describe: 'Titre de la note',
+  alias: 't',
+  required: true
+};
+
+const argv = yargs
+  .command('add', 'Ajouter une note', { title: titleOption})
+  .command('list', 'Afficher la liste des commandes')
+  .command('edit', 'Modifier une note', { 
+    title: titleOption,
+    newTitle: {
+      describe: 'Nouveau titre de la note',
+      required: true
+    }
+  })
+  .command('remove', 'Supprimer une note', { title: titleOption })
+  .help()
+  .argv;
 
 const cmd = argv._[0]; // récupère le nom de la commande
-console.log(cmd);
 
+if (cmd === 'add') {
+  addNote(argv.title);
+  console.log('[+] Note enregistrée');
+} else if (cmd === 'list') {
+  let notes = fetchNotes();
+  console.log('[+] Liste des notes enregistrées');
+  notes.forEach(note => console.log(note.title))
+} else if (cmd === 'edit') {
+  let result = editNote(argv.title, argv.newTitle);
+  if (result) {
+    console.log('[+] Note mise à jour')
+  } else {
+    console.log('[-] Aucune note portant ce titre n\'a a été trouvée');
+  }
+} else if (cmd === 'remove') {
+  let result = removeNote(argv.title);
+  if (result) {
+    console.log('[+] La note a été supprimée');
+  } else {
+    console.log('[-] La note n\'a pas été supprimée');
+  }
+} else {
+  console.log('[-] Commande non reconnue');
+}
 
-//addNote();
--- node-notes/notes.js
+++ node-notes/notes.js
@@ -1,11 +1,47 @@
 const fs = require('fs'); // module natif
 
-const addNote = () => {
-  console.log("Ajout d'une note");
+const NOTES_FILE = 'notes-data.json';
 
-  fs.writeFileSync('demo.txt', 'coucou');
+const fetchNotes = () => {
+  let notes = fs.readFileSync(NOTES_FILE);
+  return JSON.parse(notes);
+};
+
+const addNote = (title) => {
+  let notes = fetchNotes(); // récupération des notes enregistrées
+  notes.push({title});
+  saveNotes(notes);
+};
+
+const editNote = (title, newTitle) => {
+  let notes = fetchNotes();
+
+  for (let i=0; i<notes.length; i++) {
+    if (notes[i].title === title) {
+      notes[i].title = newTitle; // update
+      saveNotes(notes); // update sur disque
+      return true;
+    }
+  }
+
+  return false;
+
+};
+
+const removeNote = (title) => {
+  let notes = fetchNotes();
+  let newNotes = notes.filter(note => note.title !== title);
+  saveNotes(newNotes);
+  return notes.length !== newNotes.length; // retourne booléen
+};
+
+const saveNotes = (notes) => {
+  fs.writeFileSync(NOTES_FILE, JSON.stringify(notes));
 }
 
 module.exports = {
-  addNote
+  addNote,
+  fetchNotes,
+  editNote,
+  removeNote
 }
