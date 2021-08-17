db976be5a7113ed2ac55c69e2a8740cd5d14528b
cdufour <opusidea@gmail.com>
2021-08-17 16:04:30 +0200

    j2 tp

--- node-webserver/express-demo/index.js
+++ node-webserver/express-demo/index.js
@@ -1,6 +1,12 @@
 const express = require('express');
 const app = express();
 
+// connecte l'app express au moteur de rendu pug
+app.set('view engine', 'pug');
+
+// Middleware
+app.use(express.urlencoded({ extended: true }));
+
 app.get('/', (req, res) => {
   //res.set('Content-Type', 'text/plain');
   res.set({
@@ -12,6 +18,48 @@ app.get('/', (req, res) => {
 
 app.get('/notfound', (req, res) => {
   res.status(404).send('Je n\'existe pas :-(((');
+})
+
+app.get('/teams', (req, res) => {
+  const title = 'teams';
+  const html = `
+    <!DOCTYPE html>
+    <html>
+      <head>
+        <title>${title}</title>
+      </head>
+      <body>
+        <h1>${title}</h1>
+      </body>
+    </html>
+  `;
+  res.send(html);
+})
+
+// Exemple d'utlisation de la Query String
+app.get('/search', (req, res) => {
+  const {team} = req.query;
+  //res.set('Content-Type', 'application/json')
+  //res.send(JSON.stringify({equipe: team}));
+  // les deux lignes ci-dessus peuvent être résumées comme suit:
+  res.json({equipe: team});
+})
+
+app.get('/product/:id', (req, res) => {
+  res.send(req.params.id);
+})
+
+app.get('/juve/logo', (req, res) => {
+  res.sendFile(__dirname + '/static/images/juve.jpg');
+})
+
+app.get('/login', (req, res) => {
+  res.render('login', { message: 'Formulaire de login', active: true });
+})
+
+app.post('/login', (req, res) => {
+  const {email, password} = req.body;
+  res.json({email, password});
 })
 
 app.listen(3300, () => {
